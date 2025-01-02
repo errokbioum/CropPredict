6d76763a6cd0b115ef74da8b272495d56875e3bb
OUMAIMA <165966722+errokbioum@users.noreply.github.com>
2025-01-03 00:56:30 +0100

    Update plot.py

--- apiPrediction/plot.py
+++ apiPrediction/plot.py
@@ -1,28 +1,4 @@
-"""
-from tensorflow.keras.models import load_model
-import pickle
-import joblib
 
-crop_recommendation_model_path = './models/RandomForest.pkl'
-crop_recommendation_model = joblib.load(crop_recommendation_model_path)
-
-def topCrops(crops, data):
-    # Utilisation de la méthode predict pour obtenir les probabilités des cultures
-    probs = crop_recommendation_model.predict(data)
-    
-    # Dictionnaire pour stocker les probabilités des cultures
-    crop_probs = {}
-    for i in range(len(crops)):
-        # Convertir la probabilité en float pour éviter l'erreur de sérialisation
-        crop_probs[crops[i]] = float(probs[0][i])  # Convertir en float
-    
-    # Trier les cultures par probabilité décroissante et garder les 5 meilleures
-    top_crops = sorted(crop_probs.items(), key=lambda x: x[1], reverse=True)[:5]
-    top_crops = dict(top_crops)  # Transformer en dictionnaire
-
-    return top_crops
-    
-"""
 
 import pickle
 crop_recommendation_model_path = './models/RandomForest.pkl'
